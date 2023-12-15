dc19cfe1161f3ddc9277fe8eed4422e5d0ba11ee
Lorenzzz90 <lorenzomarziali90@gmail.com>
2023-12-15 17:12:43 +0100

    first implementation

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-	fmt.Printf("Hello world!")
+
+	var characters string = ""
+	if len(os.Args) > 2 {
+		fmt.Println("You should provide zero or one command line arguments.")
+	} else if len(os.Args) == 1 {
+		characters = "abcdefgh"
+	} else {
+		characters = os.Args[1]
+	}
+	for i := 0; i < 8; i++ {
+		fmt.Printf("%c", characters[0])
+	}
 }
