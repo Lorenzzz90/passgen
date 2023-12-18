fb762de2a54c9511d86155f4718207e62d56e041
ossan-dev <ivan.pesenti.dev@gmail.com>
2023-12-18 08:58:26 +0100

    2023-12-18 review

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-
 	var characters string = ""
 	if len(os.Args) > 2 {
+		// FIXME: if you give a wrong number of arguments, it continues and reports a panic
+		// immediately stop the program as soon as you hit a blocker for your program.
 		fmt.Println("You should provide zero or one command line arguments.")
 	} else if len(os.Args) == 1 {
 		characters = "abcdefgh"
@@ -18,6 +21,7 @@ func main() {
 		characters = characters + string(char)
 	}
 
+	// FIXME: print a better message. Not only a password. You have to write meaningful UI (the user should not guess what you're doing!)
 	for i := 0; i < 8; i++ {
 		fmt.Printf("%c", characters[i])
 	}
